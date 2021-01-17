c8e3f69ec919ea6cd4c622c7b48b003913429b71
ChrisVS-9 <ckrizvs@hotmail.com>
2021-01-17 00:33:27 -0600

    ultimo commit

---
+++ models/respuestaConsultaRelacion.go
@@ -0,0 +1,5 @@
+package models
+/*RespuestaConsultaRelacion tiene el true o false que se obtiene de consultar la relacion entre 2 usuarios*/
+type RespuestaConsultaRelacion struct{
+	Status bool `json:"status"`
+}
